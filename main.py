1d5f9730f1e0756839e68801d1118660a708ba87
Jalegosa <jalegosa01@gmail.com>
2025-07-11 21:09:30 -0600

    class

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+class Alumno:
+    def __init__(self, nombre, carnet, carrera, nota_final):
+        self.nombre = nombre
+        self.carnet = carnet
+        self.carrera = carrera
+        self.nota_final = nota_final
