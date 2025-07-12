2d6ff9d03a57d1c0adfecb346e3c6fb8855adf79
Jalegosa <jalegosa01@gmail.com>
2025-07-11 21:11:44 -0600

    ver info

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ class Alumno:
         self.carnet = carnet
         self.carrera = carrera
         self.nota_final = nota_final
+
+    def mostrar_info(self):
+        print(f"\nNombre Completo: ´{self.nombre}")
+        print(f"Carnet: {self.carnet}")
+        print(f"Carrera: {self.carrera}")
+        print(f"Nota final:{self.nota_final} ")
