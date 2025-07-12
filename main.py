486df083aaf9cc72da3b5db192f0ce8ba09b4e9e
Jalegosa <jalegosa01@gmail.com>
2025-07-11 21:18:26 -0600

    ver info

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ class Alumno:
         print(f"Carnet: {self.carnet}")
         print(f"Carrera: {self.carrera}")
         print(f"Nota final:{self.nota_final} ")
+
+class sis:
+    def __init__(self):
+        self.alumnos = []
+
+    def registro_alumnos(self):
+        print("\n REGISTRO")
+        nombre = input("Por favor ingrese el nombre: ")
+        carnet = input("Por favor ingrese el número de carnet: ")
+        carrera = input ("Por favor ingrese su carrera ")
+        Nota_final = input("Por favor ingrese la nota ")
+
+    def mostrar_alumnos(self):
+
+
+
